f0327e8a15c8d32f7bbdfd4507c2a23b148f5a25
Roman Timoshkin <rmntim@icloud.com>
2025-06-14 19:18:04 +0300

    test: update to check memory dump

--- python/golden_test.py
+++ python/golden_test.py
@@ -16,6 +16,7 @@ def run_command(command):
 def test_translator_and_machine(golden):
     with tempfile.TemporaryDirectory() as tmpdirname:
         source = os.path.join(tmpdirname, "source.sasm")
+        memory_dump = os.path.join(tmpdirname, "source.debug")
         input_stream = os.path.join(tmpdirname, "input.txt")
         output_stream = os.path.join(tmpdirname, "output.txt")
         log_stream = os.path.join(tmpdirname, "comp_log.txt")
@@ -27,6 +28,10 @@ def test_translator_and_machine(golden):
             file.write(golden["in_stdin"])
 
         run_command(f'./gradlew asm:run --args="{source} {target}"')
+
+        with open(memory_dump, encoding="utf-8") as file:
+            memory = file.read()
+
         run_command(f'./gradlew comp:run --args="-p {target} -i {input_stream} -o {output_stream} -l {log_stream}"')
 
         with open(output_stream, encoding="utf-8") as file:
@@ -36,4 +41,5 @@ def test_translator_and_machine(golden):
             log = "\n".join(file.readlines()[:LOG_LINE_LIMIT])
 
         assert output == golden.out["out_stdout"]
+        assert memory == golden.out["memory"]
         assert log == golden.out["out_log"]
